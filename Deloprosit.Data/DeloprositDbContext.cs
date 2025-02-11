4bc51d7bc5de5d8cea1f070c524b3c54586a5155
V.Rumyantsev <V.Rumyantsev@48-1335>
2025-02-11 18:59:15 +0300

    Added Data class library

---
+++ Deloprosit.Data/DeloprositDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Deloprosit.Data
+{
+    public class DeloprositDbContext: DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public DeloprositDbContext(DbContextOptions<DeloprositDbContext> options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration["ConnectionStrings:DeloprositDb"]);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
--- Deloprosit.Server/Program.cs
+++ Deloprosit.Server/Program.cs
@@ -1,11 +1,57 @@
-using Microsoft.AspNetCore.Cors.Infrastructure;
+using Deloprosit.Data;
 
+using JsonFlatFileDataStore;
+
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+var jsonFileCreated = false;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddLogging(logs =>
+{
+    logs.AddConsole();
+});
 
 builder.Services.AddControllers();
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme);
+builder.Services.AddAuthorization();
+
 builder.Services.AddCors(options => options.AddPolicy("AllowAll",
     new CorsPolicyBuilder().AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().Build()));
+
+if (builder.Environment.IsDevelopment())
+{
+    var connectionString = builder.Configuration.GetConnectionString("DeloprositDb");
+    builder.Services.AddDbContext<DeloprositDbContext>(options => options.UseSqlServer(connectionString));
+}
+else
+{
+    var path = $"{Directory.GetCurrentDirectory()}\\DeloprositDbJson\\";
+
+    if (!Directory.Exists(path))
+    {
+        Directory.CreateDirectory(path);
+    }
+
+    path += "deloprositDb.json";
+
+    if (!File.Exists(path))
+    {
+        var stream = File.Create(path);
+        stream.Close();
+        File.WriteAllText(path, "{}");
+        jsonFileCreated = true;
+    }
+
+    builder.Services.AddScoped(serviceProvider => new DataStore(path, useLowerCamelCase: false));
+}
+
+var provider = builder?.Services?.BuildServiceProvider();
+using var scope = provider?.CreateScope();
+await MigrateSeedDatabase(scope, jsonFileCreated);
 
 var app = builder.Build();
 
@@ -17,9 +63,32 @@ app.UseStaticFiles();
 app.UseHttpsRedirection();
 
 app.UseCors();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+async Task MigrateSeedDatabase(IServiceScope? scope, bool jsonFileCreated)
+{
+    if (builder!.Environment.IsDevelopment())
+    {
+        var dbContext = scope?.ServiceProvider.GetRequiredService<DeloprositDbContext>();
+        dbContext?.Database.Migrate();
+    }
+    else if (jsonFileCreated)
+    {
+        var dataStore = scope?.ServiceProvider.GetRequiredService<DataStore>() ?? throw new ArgumentNullException("Could not get DataStore from DI");
+        await SeedJsonDb(dataStore, builder.Configuration);
+    }
+}
+
+async Task SeedJsonDb(DataStore dataStore, IConfiguration configuration)
+{
+    var adminEmail = configuration["AdminEmail"] ?? throw new ArgumentNullException("AdminEmail is null.");
+    var adminPassword = configuration["AdminPassword"] ?? throw new ArgumentNullException("AdminPassword is null.");
+
+}
