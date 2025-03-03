a91f7fc5d8aec6557bacf3d04cb01e94224b569e
DarthVader666666 <rumyancer@gmail.com>
2025-03-04 00:47:14 +0300

    Added static files

--- deloprosit.client/src/vuex/store.js
+++ deloprosit.client/src/vuex/store.js
@@ -12,14 +12,25 @@ const store = createStore({
         showSearchBar: true,
         title: null,
         isEditMode: false,
-        imagePaths:
-        [
-            '/src/assets/images/archive-1.png',
-            '/src/assets/images/case-files-1.png',
-            '/src/assets/images/folders-1.png',
-            '/src/assets/images/laptop-1.png',
-            '/src/assets/images/laptop-2.png',
-        ]
+        imagePaths: (
+            import.meta.env.VITE_API_ENVIRONMENT === 'development' 
+            ? 
+            [
+                '/src/assets/images/archive-1.png',
+                '/src/assets/images/case-files-1.png',
+                '/src/assets/images/folders-1.png',
+                '/src/assets/images/laptop-1.png',
+                '/src/assets/images/laptop-2.png',
+            ]
+            :
+            [
+                'archive-1.png',
+                'case-files-1.png',
+                'folders-1.png',
+                'laptop-1.png',
+                'laptop-2.png',
+            ])
+        
     },
     getters: {
         serverUrl(state) {
