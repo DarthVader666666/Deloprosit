109116acf97cafd4fc397bcea84d2ed91e3ff005
V.Rumyantsev <V.Rumyantsev@48-1335>
2025-03-27 18:17:53 +0300

    Resolved event conflict

--- deloprosit.client/src/components/RightColumn.vue
+++ deloprosit.client/src/components/RightColumn.vue
@@ -15,7 +15,6 @@ const isAdmin = computed(() => store.getters.isAdmin);
 const isOwner = computed(() => store.getters.isOwner);
 const documentNodes = computed(() => store.getters.getDocumentNodes);
 
-const selectedKey = ref(null);
 const showNewFolderMenu = ref(false);
 const showUploadMenu = ref(false);
 const newFolderName = ref(null);
@@ -166,21 +165,14 @@ async function deleteFile(filePath) {
                 </div>
             </div>
             <hr/>
-            <Tree :value="documentNodes" class="tree" v-model:selectionKeys="selectedKey" selectionMode="single" @nodeSelect="download">
+            <Tree :value="documentNodes" class="tree">
                 <template #url="{ node }">
                     <div>
-                        <span>{{ node.label }}</span>
-                        <Button v-if="isAdmin || isOwner" @click="deleteFile(node.key)" severity="danger" text rounded icon="pi pi-times"/>
+                        <span @click="download(node)" class="file-name">{{ node.label }}</span>
+                        <Button v-if="isAdmin || isOwner" @click="deleteFile(node.key)" severity="danger" text rounded title="Удалить файл" icon="pi pi-times"/>
                     </div>
                 </template>
             </Tree>
-            <!-- <div class="link" v-for="(document, index) in documents" :key="index">
-                <span @click.prevent="download(document.path, document.name)">
-                    <i class="pi pi-file"></i>
-                    {{ document.name }} 
-                </span>                
-                <Button v-if="isAdmin || isOwner" @click.prevent="deleteFile(document.name)" severity="danger" text rounded><i class="pi pi-times"></i></Button>
-           </div> -->
         </div>
     </div>        
 </template>
@@ -245,6 +237,11 @@ async function deleteFile(filePath) {
     
     .tree:deep(span button span) {
         font-size: x-small;
+    }
+
+    .tree:deep(.file-name:hover) {
+        cursor: pointer;
+        text-decoration: underline;
     }
 
     .right-column-menu {
