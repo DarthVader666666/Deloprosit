ea8b0fe71f1e7cb11bfbb06e30b32f7c05e4d364
DarthVader666666 <rumyancer@gmail.com>
2025-04-03 22:58:12 +0300

    Worked on documents column

--- deloprosit.client/src/components/MainComponent.vue
+++ deloprosit.client/src/components/MainComponent.vue
@@ -9,21 +9,18 @@ import { computed, ref } from 'vue';
 const store = useStore();
 const title = computed(() => store.state.title);
 const showRightColumn = ref(false);
-const topPosition = ref(0);
 
 function showDocuments() {
     showRightColumn.value = true;
 
     const centralComponent = document.getElementById('central-component');
-    topPosition.value = document.getElementById('app').offsetTop;
-    console.log(topPosition.value)
-    centralComponent.style.display = 'none';
+    centralComponent.style.position = 'absolute';
 
     const rightColumn = document.getElementById('right-column');
+    rightColumn.style.position = 'fixed';
+    rightColumn.style.right = '0';
     rightColumn.style.display = 'block';
-    rightColumn.style.width = '100%';
-
-    window.scrollTo(0, 0);
+    rightColumn.style.width = '360px';
 }
 
 function hideDocuments() {
@@ -31,11 +28,10 @@ function hideDocuments() {
 
     const centralComponent = document.getElementById('central-component');
     centralComponent.style.display = 'block';
+    centralComponent.style.removeProperty('position');
 
     const rightColumn = document.getElementById('right-column');
     rightColumn.style.display = 'none';
-
-    window.scrollTo(0, topPosition.value);
 }
 
 </script>
@@ -46,20 +42,32 @@ function hideDocuments() {
         <LeftColumnView/>
         <RouterView class="central-container" id="central-component"/>
         <RightColumnView/>
-        <Button v-if="!showRightColumn" @click="showDocuments" class="document-button" severity="secondary" raised icon="pi pi-caret-left"></Button>
-        <Button v-else @click="hideDocuments" class="document-button" severity="contrast" raised icon="pi pi-caret-right"></Button>
+        <div class="document-button">
+            <Button v-if="!showRightColumn" @click="showDocuments"  severity="secondary" raised icon="pi pi-caret-left"></Button>
+            <Button v-else @click="hideDocuments" severity="contrast" raised icon="pi pi-caret-right"></Button>
+        </div>
     </div>
 </template>
 
 <style scoped>
 .document-button {
     position: fixed;
-    height: 60px;
-    width: 15px;
     margin-top: 25vh;
     margin-bottom: auto;
     right: 0;
+    display: none;
+}
+
+.document-button button {
+    height: 60px;
+    width: 15px;
     border-radius: 50% 0 0 50%;
+}
+
+@media (max-width: 1100px) {
+    .document-button {
+        display: block;
+    }
 }
 
 </style>
