bdbb870843565b54b90e0b90eff5d36ef21ad970
DarthVader666666 <rumyancer@gmail.com>
2025-04-15 10:59:39 +0300

    Suppressed not awaited call warning

--- Deloprosit.Bll/Services/GoogleDriveService.cs
+++ Deloprosit.Bll/Services/GoogleDriveService.cs
@@ -20,7 +20,9 @@ namespace Deloprosit.Bll.Services
 
             try
             {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                 DownloadFolderContentsAsync(ConfigurationHelper.GoogleDriveFolderId, ConfigurationHelper.DocsPath);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             }
             catch (Exception ex)
             {
