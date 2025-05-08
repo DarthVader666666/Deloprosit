6d704e15687e28305364b0f155a7d69efa939eaf
DarthVader666666 <rumyancer@gmail.com>
2025-05-08 20:08:08 +0300

    Added migration for postgres

--- Delopro.Data/Migrations/PostgresDeloproDb/20250508170709_Added_Deletion_fields_to_User.Designer.cs
+++ Delopro.Data/Migrations/PostgresDeloproDb/20250508170709_Added_Deletion_fields_to_User.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Delopro.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Delopro.Data.Migrations.PostgresDeloproDb
 {
     [DbContext(typeof(PostgresDeloproDbContext))]
-    partial class PostgresDeloproDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250508170709_Added_Deletion_fields_to_User")]
+    partial class Added_Deletion_fields_to_User
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Delopro.Data/Migrations/PostgresDeloproDb/20250508170709_Added_Deletion_fields_to_User.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Delopro.Data.Migrations.PostgresDeloproDb
+{
+    /// <inheritdoc />
+    public partial class Added_Deletion_fields_to_User : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletionDate",
+                table: "Users",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "DeletionDate",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2,
+                column: "DeletionDate",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletionDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Users");
+        }
+    }
+}
